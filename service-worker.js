1308e51d3cdccf29bb86bdbb31025801df302be7
Hariom Tripathi <hariomtripathi1507@gmail.com>
2026-06-13 01:42:14 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ const FILES_TO_CACHE = [
   "./style.css",
   "./app.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icon-192.png",
+  "./icon-512.png",
 ];
 
 // Install: cache all the core files
